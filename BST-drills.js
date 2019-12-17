fe89c0127b4f6687f343b4b34997446a3e4f8cc3
danWagar <dandwagar@gmail.com>
2019-12-17 13:22:26 -0800

    compare bst

--- BST-drills.js
+++ BST-drills.js
@@ -1,3 +1,4 @@
+/* eslint-disable strict */
 let BinarySearchTree = require('./BinarySearchTree');
 
 function main() {
@@ -68,20 +69,18 @@ function testFind() {
 
 function isItABST(t) {
   if (!t) {
-    return;
+    return true;
   }
-  if (t.key > t && t.left) {
+  if (t.left && t.left.key > t.key) {
     return false;
   }
-  if (t.right < t) {
+  if (t.right && t.right.key < t.key) {
     return false;
   }
   let left = isItABST(t.left);
   let right = isItABST(t.right);
-  if (!left && !right) {
-    return true;
-  }
-  return false;
+
+  return left && right;
 }
 
 function testIsItBST() {
@@ -95,9 +94,138 @@ function testIsItBST() {
   BST.insert(5);
   BST.insert(7);
   console.log(isItABST(BST));
-  //   let t = BST.find(7);
-  //   console.log(t);
-  //   t.left = new BinarySearchTree(20, null, t);
-  //   console.log(isItABST(BST), 'w/ test case');
+  let t = BST.find(7);
+  t.left = new BinarySearchTree(20, null, t);
+  console.log(isItABST(BST), 'w/ test case');
 }
-testIsItBST();
+//testIsItBST();
+//
+function thirdLargest(t) {
+  if (!t) {
+    return t.parent.parent;
+  }
+
+  farRight = getMax(t);
+  //check if farRight has multiple left -> return second left;
+  if (farRight.left.left) return farRight.left.left;
+
+  //if farRight only has one left -> return parent
+  if (farRight.left) return farRight.parent;
+
+  //check if parent has left -> return first left
+  if (farRight.parent.left) return farRight.parent.left;
+
+  //return parents parent;
+  return farRight.parent.parent;
+}
+
+function getMax(t) {
+  if (!t.right) return t;
+  return getMax(t.right);
+}
+
+function testThirdLargest() {
+  //test second case
+  let BST = new BinarySearchTree();
+  BST.insert(3);
+  BST.insert(1);
+  BST.insert(4);
+  BST.insert(6);
+  BST.insert(9);
+  BST.insert(2);
+  BST.insert(5);
+  BST.insert(7);
+  console.log(thirdLargest(BST));
+}
+
+// function isBalanced(t, depth = 1, leafDepth = []) {
+//   if (!t) {
+//     leafDepth.push(depth);
+//     return depth;
+//   }
+
+//   let lDepth = isBalanced(t.left, depth++, leafDepth);
+//   let rDepth = isBalanced(t.right, depth++, leafDepth);
+
+//   return leafDepth;
+// }
+
+//testThirdLargest();
+
+function isBalanced(t) {
+  let lMaxDepth = findHeight(t.left);
+  let rMaxDepth = findHeight(t.right);
+  console.log('left: ' + lMaxDepth);
+  console.log('right: ' + rMaxDepth);
+  if (Math.abs(lMaxDepth - rMaxDepth) > 1) return false;
+  return true;
+}
+
+function testIsBalanced() {
+  let ASCI = new BinarySearchTree();
+  ASCI.insert('E');
+  ASCI.insert('A');
+  ASCI.insert('S');
+  ASCI.insert('Y');
+  ASCI.insert('Q');
+  ASCI.insert('U');
+  ASCI.insert('E');
+  ASCI.insert('S');
+  ASCI.insert('T');
+  ASCI.insert('I');
+  ASCI.insert('O');
+  ASCI.insert('N');
+  console.log(isBalanced(ASCI));
+
+  let BST = new BinarySearchTree();
+  BST.insert(3);
+  BST.insert(1);
+  BST.insert(4);
+  BST.insert(6);
+  BST.insert(9);
+  BST.insert(2);
+  BST.insert(5);
+  BST.insert(7);
+  // let t = BST.find(7);
+  // t.left = new BinarySearchTree(20, null, t);
+  console.log(isBalanced(BST));
+}
+
+//testIsBalanced();
+
+function compareBST(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  if (arr1[0] !== arr2[0]) return false;
+  let bstAsArr1 = [arr1[0]];
+  let bstAsArr2 = [arr2[0]];
+
+  for (let i = 1; i < arr1.length; i++) {
+    addToTree(bstAsArr1, 0, arr1[i]);
+    addToTree(bstAsArr2, 0, arr1[i]);
+  }
+
+  console.log(bstAsArr1);
+  console.log(bstAsArr2);
+}
+
+function addToTree(t, root, val) {
+  if (val < t[root]) {
+    let lIndex = root * 2 + 1;
+    if (!t[lIndex]) {
+      t[lIndex] = val;
+      return;
+    }
+    addToTree(t, lIndex, val);
+  }
+  if (val >= t[root]) {
+    let rIndex = root * 2 + 2;
+    if (!t[rIndex]) {
+      t[rIndex] = val;
+      return;
+    }
+    addToTree(t, rIndex, val);
+  }
+  return;
+}
+
+compareBST([3, 5, 4, 6, 1, 0, 2], [3, 1, 5, 2, 4, 6, 0]);
